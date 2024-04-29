75cc7c7dde281e91fdcb1247d412a1b502a20985
M4tryxXx <M4tryxxx@gmail.com>
2024-04-29 19:26:51 +0100

    Created Main Design
    Created main design and made an idea  of how it should run

---
+++ resources/main.js
@@ -0,0 +1,33 @@
+
+
+//Declaring variables
+let messageNumber = document.getElementById('quantity');
+let contentBox = document.getElementById('instructions');
+const drawBtn = document.getElementById('draw-button');
+const goBack = document.getElementById('back-button');
+
+//Create an event listener for Draw button
+
+drawBtn.addEventListener('click', () => {
+    messageNumber.innerHTML = 'Message no: #';
+    let message = document.createElement('h2');
+    message.style = 'color: #001530; text-shadow: 2px 2px 5px #4200008c;';
+    message.innerHTML = 'This will be the message drawn!';
+    contentBox.innerHTML = '';
+    contentBox.appendChild(message);
+    drawBtn.style = 'display: none;';
+    goBack.style = 'display: block;';
+    buttonAllign.style = 'text-align: center;';
+});
+
+
+goBack.addEventListener('click', () => {
+    messageNumber.innerHTML = '360 Draws';
+    let afterMessage = document.createElement('h2');
+    afterMessage.style = 'color: #001530; text-shadow: 2px 2px 5px #4200008c;';
+    afterMessage.innerHTML = 'Do you wanna try another one?';
+    contentBox.innerHTML = '';
+    contentBox.appendChild(afterMessage);
+    drawBtn.style = 'display: block;';
+    goBack.style = 'display: none;';
+})
